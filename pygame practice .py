30aacd5fb675d66f75e953e3f5a19127e8aa066c
Matthew.O <70594967+s11137437@users.noreply.github.com>
2021-11-29 09:01:32 -0500

    v0.0

---
+++ pygame practice .py
@@ -0,0 +1,11 @@
+# pygame Practice, O'Berry Matthew, 11/29/21 8:37, v0.0
+
+import pygame, sys
+from pygame.locals import *
+
+# start pygame
+pygame.init()
+
+# creat game window
+windowsurface = pygame.display.set_mode((500, 400),0, 32)
+pygame.display.set_caption("Hello, World!")
